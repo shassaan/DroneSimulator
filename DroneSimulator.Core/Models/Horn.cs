1afd78bac97ab4765e2ac1874f4958000355c044
shassaan <syedhassaan.103@gmail.com>
2020-11-25 00:25:29 +0500

    added interface Iswichable

--- DroneSimulator.Core/Models/Horn.cs
+++ DroneSimulator.Core/Models/Horn.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DroneSimulator.Core.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +7,8 @@ using System.Threading.Tasks;
 
 namespace DroneSimulator.Core.Models
 {
-    public class Horn
+    public class Horn : ISwitchable
     {
-        public bool Switch { get; set; }
+        public bool Switch { get ; set ; }
     }
 }
