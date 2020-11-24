1c607ab3f9b708f3f1fc687c4a032e6c67bac6af
shassaan <syedhassaan.103@gmail.com>
2020-11-24 23:38:59 +0500

    Add project files.

---
+++ DroneSimulator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DroneSimulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
