a12d8da324cd9a11aa7d23599af725f5edf00cb4
shassaan <syedhassaan.103@gmail.com>
2020-11-27 18:40:28 +0500

    changed the target framework to netcore3.1

--- DroneSimulator/Program.cs
+++ DroneSimulator/Program.cs
@@ -4,41 +4,50 @@ using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Threading;
 
-try
+namespace DroneSimulator
 {
-    var serviceCollection = new ServiceCollection();
-    serviceCollection.RegisterDroneServices();
-    var DroneService = serviceCollection.BuildServiceProvider().GetService<IDroneSimulatorService>();
-    DroneService.Start(
-        new Boundary
+    public static class Program
+    {
+        public static void Main(string[] arg)
         {
-            Px_Y = new System.Drawing.Point(0, 0),
-            Bx_Y = new System.Drawing.Point(0, 0)
-        },
-        DroneSimulator.Core.DroneConstants.NavigationModules.RAILS
-        );
+            try
+            {
+                var serviceCollection = new ServiceCollection();
+                serviceCollection.RegisterDroneServices();
+                var DroneService = serviceCollection.BuildServiceProvider().GetService<IDroneSimulatorService>();
+                DroneService.Start(
+                    new Boundary
+                    {
+                        Px_Y = new System.Drawing.Point(0, 0),
+                        Bx_Y = new System.Drawing.Point(0, 0)
+                    },
+                    DroneSimulator.Core.DroneConstants.NavigationModules.RAILS
+                    );
 
 
-    DroneService.Alert(3000);
+                DroneService.Alert(3000);
 
 
-    var FlashCTS = new CancellationTokenSource();
-    DroneService.FlashLights(FlashCTS.Token);
-    Thread.Sleep(5000);
-    FlashCTS.Cancel();
+                var FlashCTS = new CancellationTokenSource();
+                DroneService.FlashLights(FlashCTS.Token);
+                Thread.Sleep(5000);
+                FlashCTS.Cancel();
 
 
-    var ToggleCTS = new CancellationTokenSource();
-    DroneService.ToggleLight(ToggleCTS.Token);
-    Thread.Sleep(5000);
-    ToggleCTS.Cancel();
+                var ToggleCTS = new CancellationTokenSource();
+                DroneService.ToggleLight(ToggleCTS.Token);
+                Thread.Sleep(5000);
+                ToggleCTS.Cancel();
 
 
-    DroneService.Move(3, 91);
+                DroneService.Move(3, 91);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+    }
 }
-catch (Exception ex)
-{
-    Console.WriteLine(ex.Message);
-}
-
 
